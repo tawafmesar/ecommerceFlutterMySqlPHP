0b0f54647d89d6379c5f04cf496b17f0761c669d
tawafmesar <83729930+tawafmesar@users.noreply.github.com>
2023-03-20 04:10:32 +0400

    4 - onboarding part 1  add images and font ,create onboarding paje

---
+++ lib/view/screen/onboarding.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class onBoarding extends StatelessWidget {
+  const onBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body:  Container(
+        padding: const EdgeInsets.all(10),
+        child: const Text('Hi'),),
+    );
+  }
+}
