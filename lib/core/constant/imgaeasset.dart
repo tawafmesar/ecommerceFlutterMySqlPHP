ccf17176159ba7f3546b2ee9820fbea64672d55d
tawafmesar <83729930+tawafmesar@users.noreply.github.com>
2023-03-25 01:02:13 +0400

    5 - onboarding part 2 complite some things

---
+++ lib/core/constant/imgaeasset.dart
@@ -0,0 +1,12 @@
+class AppImageAsset {
+
+  static const String rootImage = "assets/images";
+
+
+  static const String onBoardingImageOne = "$rootImage/onboardingone.PNG";
+  static const String onBoardingImageTwo = "$rootImage/onboardingtwo.PNG";
+  static const String onBoardingImageThree = "$rootImage/onboardingthree.PNG";
+  static const String onBoardingImageFour = "$rootImage/onboardingfour.PNG";
+
+
+}
--- lib/view/screen/onboarding.dart
+++ lib/view/screen/onboarding.dart
@@ -1,4 +1,6 @@
 
+import 'package:ecommerce_flutter_php_mysql/core/constant/color.dart';
+import 'package:ecommerce_flutter_php_mysql/data/datasource/static/static.dart';
 import 'package:flutter/material.dart';
 
 class onBoarding extends StatelessWidget {
@@ -8,9 +10,35 @@ class onBoarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       
-      body:  Container(
-        padding: const EdgeInsets.all(10),
-        child: const Text('Hi'),),
+      body: SafeArea(
+        child: PageView.builder(
+          
+          itemCount: onBoardingList.length,
+          itemBuilder:(context,i) =>   Column(
+            children: [
+                Text(onBoardingList[i].title!,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,fontSize: 20),) ,
+                  const SizedBox(height: 80,),
+                Image.asset(onBoardingList[i].image!
+                ,width: 200,height: 250,fit: BoxFit.fill,
+                ),
+                  const SizedBox(height: 80,),
+                Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: Text(onBoardingList[i].body!,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(height: 2,color: AppColor.grey,fontWeight: FontWeight.bold,fontSize: 17),
+                   )
+                  ),
+                  
+            ],
+
+          )),
+      )
+
+      
     );
   }
 }
