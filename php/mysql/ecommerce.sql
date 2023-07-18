136c54b529ca94f16560c696018c28b95951f625
tawafmesar <83729930+tawafmesar@users.noreply.github.com>
2023-07-19 03:13:14 +0400

    52 & 53 categories and items tables (databasw)

---
+++ php/mysql/ecommerce.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 19, 2023 at 01:11 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `categories_id` int(11) NOT NULL,
+  `categories_name` varchar(100) NOT NULL,
+  `categories_name_ar` varchar(100) NOT NULL,
+  `categories_image` varchar(255) NOT NULL,
+  `categories_datetime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `items_id` int(11) NOT NULL,
+  `items_name` varchar(100) NOT NULL,
+  `items_name_at` varchar(100) NOT NULL,
+  `items_desc` varchar(255) NOT NULL,
+  `items_desc_ar` varchar(255) NOT NULL,
+  `items_image` varchar(255) NOT NULL,
+  `items_count` int(11) NOT NULL,
+  `items_active` tinyint(4) NOT NULL DEFAULT 1,
+  `items_price` float NOT NULL,
+  `items_discount` smallint(6) NOT NULL,
+  `items_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `items_cat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `users_id` int(11) NOT NULL,
+  `users_name` varchar(100) NOT NULL,
+  `users_password` varchar(255) NOT NULL,
+  `users` int(11) NOT NULL,
+  `users_email` varchar(100) NOT NULL,
+  `users_phone` varchar(100) NOT NULL,
+  `users_verfiycode` int(11) NOT NULL,
+  `users_approve` tinyint(4) NOT NULL DEFAULT 0,
+  `users_create` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`users_id`, `users_name`, `users_password`, `users`, `users_email`, `users_phone`, `users_verfiycode`, `users_approve`, `users_create`) VALUES
+(21, 'asasas', '8cb2237d0679ca88db6464eac60da96345513964', 0, 'tawaf@gmail.com', '123456789', 16681, 1, '2023-07-15 23:55:09'),
+(23, 'adddda', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 0, 'asaaas@gmail.com', '1122456789', 52493, 1, '2023-07-16 00:27:24'),
+(24, 'sasas', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 0, 'fafaffa@gmail.com', '12345521677', 36462, 1, '2023-07-16 00:38:55');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categories_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`items_id`),
+  ADD KEY `items_cat` (`items_cat`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`users_id`),
+  ADD UNIQUE KEY `users_phone` (`users_phone`),
+  ADD UNIQUE KEY `users_email` (`users_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `items_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`items_cat`) REFERENCES `categories` (`categories_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
