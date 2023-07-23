502bc54d2853bebe9334a239a31e84ca75c3d393
tawafmesar <83729930+tawafmesar@users.noreply.github.com>
2023-07-23 05:38:14 +0400

    58 categories model

--- php/functions.php
+++ php/functions.php
@@ -36,7 +36,17 @@ function getAllData($table, $where = null, $values = null)
 {
     global $con;
     $data = array();
-    $stmt = $con->prepare("SELECT  * FROM $table WHERE   $where ");
+    if ($where == null){
+
+        $stmt = $con->prepare("SELECT  * FROM $table ");
+
+    }else{
+
+        $stmt = $con->prepare("SELECT  * FROM $table WHERE   $where ");
+
+    }
+
+
     $stmt->execute($values);
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $count  = $stmt->rowCount();
